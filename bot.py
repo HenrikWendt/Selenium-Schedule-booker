18585dfc2147215d27139ffd1e010b291d09a625
Henrik <henrikwendt95@gmail.com>
2020-03-03 00:01:04 +0100

    Confirmation email added to the script

--- bot.py
+++ bot.py
@@ -2,13 +2,26 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
+
+
+"""
 sal = input("Vilken sal vill du boka? ") 
 datum2 =input("Vilket datum? (OBS bara dag och månad ex 1/3) ") 
 starttid =input("Från vilken tid? (OBS måste sluta på 15 ex 13:15) ")
 sluttid =input("Till vilken tid? (OBS måste sluta på 00 ex 18:00, man kan max boka 4 timmar) ")
 user = input("Liu-ID: ")
 password = input("Lösenord till Liu-ID: ")
+"""
+
+
+sal ="---"
+datum2 ="8/3" # (OBS bara dag och månad ex 1/3)
+starttid ="17:15" #(OBS måste sluta på 15 ex 13:15)
+sluttid ="19:00" #(OBS måste sluta på 00 ex 18:00, man kan max boka 4 timmar)
+user ="**Här ska LiuID stå"
+password ="**Lösenord till LiuID**"
 
 print()
 print()
@@ -57,10 +70,12 @@ datum3 = "')]"
 datum = datum1 + datum2 + datum3
 
 options = webdriver.ChromeOptions()
+
 driver = webdriver.Chrome('D:\Progg\Selenium-bot\chromedriver', options=options)
-"""driver.maximize_window()"""
+
 
 action = ActionChains(driver)
+
 driver.get('https://cloud.timeedit.net/liu/web/wr_stud/')
 time.sleep(0)
 driver.find_elements_by_xpath("//*[contains(text(), 'Klicka här för att logga in')]")[0].click()
@@ -73,14 +88,10 @@ time.sleep(0)
 driver.find_elements_by_xpath("//*[contains(text(), 'Studentbokning LiU')]")[0].click()
 time.sleep(0)
 driver.find_elements_by_xpath('//*[@data-param="search_text"]')[0].send_keys(sal)
-time.sleep(1)
+time.sleep(2)
 driver.find_elements_by_xpath(datum)[0].click()
-"""test = driver.find_element_by_id('objectselectionresult')
-for option in test.find_elements_by_tag_name('data-short'):
-    if option.text == datum:
-        option.click() 
-        break"""
-time.sleep(0) 
+
+time.sleep(2) 
 firstLevelMenu = driver.find_element_by_id("objectselectionresult")
 action.move_to_element(firstLevelMenu).perform()
 firstLevelMenu.click()
@@ -100,11 +111,50 @@ for option in feald.find_elements_by_tag_name('option'):
         option.click() 
         break
 
+
 driver.find_elements_by_xpath('//*[@id="continueRes2"]')[0].click()
 
-time.sleep(5)
+time.sleep(2)
 
+bookingText = driver.find_element_by_id("table0")
+
+print(bookingText.text)
+
+text = bookingText.text
+time.sleep(2)
 driver.quit()
 
-"""driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-"""
+options2 = webdriver.ChromeOptions()
+driver2 = webdriver.Chrome('D:\Progg\Selenium-bot\chromedriver', options=options2)
+action2 = ActionChains(driver2)
+driver2.get('https://outlook.office365.com/mail/inbox')
+
+
+time.sleep(2)
+driver2.find_element_by_name("loginfmt").send_keys(user+"@student.liu.se")
+driver2.find_elements_by_xpath('//*[@id="idSIButton9"]')[0].click()
+time.sleep(5)
+driver2.find_elements_by_xpath('//*[@id="passwordInput"]')[0].send_keys(password)
+driver2.find_elements_by_xpath('//*[@id="submitButton"]')[0].click()
+time.sleep(5)
+driver2.find_elements_by_xpath("//*[contains(text(), 'Nytt meddelande')]")[0].click()
+
+time.sleep(3)
+
+action2.send_keys("**Här ska bekräftelse mail stå")
+action2.send_keys(Keys.TAB)
+action2.send_keys(Keys.TAB)
+action2.send_keys(Keys.TAB)
+action2.send_keys(Keys.TAB)
+action2.send_keys("Bekräftelse på salsbokning av: " +user)
+action2.send_keys(Keys.TAB)
+action2.send_keys(text)
+action2.send_keys(Keys.TAB)
+action2.send_keys(Keys.TAB)
+action2.send_keys(Keys.ENTER)
+action2.send_keys(Keys.ENTER)
+action2.perform()
+time.sleep(2)
+
+
+driver2.quit()
