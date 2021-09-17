279c057e4149d8de88a2eaf95e8c6b708d103ee8
henwe331 <henwe331@student.liu.se>
2021-09-17 10:25:45 +0200

    Drivers updated, code tested and modified for additional functionallity of the website.

--- bot.py
+++ bot.py
@@ -16,12 +16,12 @@ password = input("Lösenord till Liu-ID: ")
 """
 
 
-sal ="---"
-datum2 ="8/3" # (OBS bara dag och månad ex 1/3)
-starttid ="17:15" #(OBS måste sluta på 15 ex 13:15)
-sluttid ="19:00" #(OBS måste sluta på 00 ex 18:00, man kan max boka 4 timmar)
-user ="**Här ska LiuID stå"
-password ="**Lösenord till LiuID**"
+sal ="ISYtan9"
+datum2 ="20/9" # (OBS bara dag och månad ex 1/3)
+starttid ="13:15" #(OBS måste sluta på 15 ex 13:15)
+sluttid ="17:00" #(OBS måste sluta på 00 ex 18:00, man kan max boka 4 timmar)
+user =""
+password =""
 
 print()
 print()
@@ -70,8 +70,8 @@ datum3 = "')]"
 datum = datum1 + datum2 + datum3
 
 options = webdriver.ChromeOptions()
-
-driver = webdriver.Chrome('D:\Progg\Selenium-bot\chromedriver', options=options)
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome('E:\\Progg\\Selenium-bot\\chromedriver', options=options)
 
 
 action = ActionChains(driver)
@@ -116,45 +116,53 @@ driver.find_elements_by_xpath('//*[@id="continueRes2"]')[0].click()
 
 time.sleep(2)
 
-bookingText = driver.find_element_by_id("table0")
-
-print(bookingText.text)
-
-text = bookingText.text
+#bookingText = driver.find_element_by_id("table0")
+#print(bookingText.text)
+#text = bookingText.text
 time.sleep(2)
+
+driver.find_elements_by_xpath('//*[@id="showsendmail"]')[0].click()
+time.sleep(2)
+driver.find_elements_by_xpath('//*[@id="sendmailbutton"]')[0].click()
+
 driver.quit()
 
-options2 = webdriver.ChromeOptions()
-driver2 = webdriver.Chrome('D:\Progg\Selenium-bot\chromedriver', options=options2)
-action2 = ActionChains(driver2)
-driver2.get('https://outlook.office365.com/mail/inbox')
+#Send Email comfirmation.
 
 
-time.sleep(2)
-driver2.find_element_by_name("loginfmt").send_keys(user+"@student.liu.se")
-driver2.find_elements_by_xpath('//*[@id="idSIButton9"]')[0].click()
-time.sleep(5)
-driver2.find_elements_by_xpath('//*[@id="passwordInput"]')[0].send_keys(password)
-driver2.find_elements_by_xpath('//*[@id="submitButton"]')[0].click()
-time.sleep(5)
-driver2.find_elements_by_xpath("//*[contains(text(), 'Nytt meddelande')]")[0].click()
-
-time.sleep(3)
-
-action2.send_keys("**Här ska bekräftelse mail stå")
-action2.send_keys(Keys.TAB)
-action2.send_keys(Keys.TAB)
-action2.send_keys(Keys.TAB)
-action2.send_keys(Keys.TAB)
-action2.send_keys("Bekräftelse på salsbokning av: " +user)
-action2.send_keys(Keys.TAB)
-action2.send_keys(text)
-action2.send_keys(Keys.TAB)
-action2.send_keys(Keys.TAB)
-action2.send_keys(Keys.ENTER)
-action2.send_keys(Keys.ENTER)
-action2.perform()
-time.sleep(2)
+"""
+    options2 = webdriver.ChromeOptions()
+    driver2 = webdriver.Chrome('E:\\Progg\\Selenium-bot\\chromedriver', options=options2)
+    action2 = ActionChains(driver2)
+    driver2.get('https://outlook.office365.com/mail/inbox')
 
 
-driver2.quit()
+    time.sleep(2)
+    driver2.find_element_by_name("loginfmt").send_keys(user+"@student.liu.se")
+    driver2.find_elements_by_xpath('//*[@id="idSIButton9"]')[0].click()
+    time.sleep(5)
+    driver2.find_elements_by_xpath('//*[@id="passwordInput"]')[0].send_keys(password)
+    driver2.find_elements_by_xpath('//*[@id="submitButton"]')[0].click()
+    time.sleep(5)
+    driver2.find_elements_by_xpath("//*[contains(text(), 'Nytt meddelande')]")[0].click()
+
+    time.sleep(3)
+
+    action2.send_keys("henrikwendt95@gmail.com")
+    action2.send_keys(Keys.TAB)
+    action2.send_keys(Keys.TAB)
+    action2.send_keys(Keys.TAB)
+    action2.send_keys(Keys.TAB)
+    action2.send_keys("Bekräftelse på salsbokning av: " +user)
+    action2.send_keys(Keys.TAB)
+    action2.send_keys(text)
+    action2.send_keys(Keys.TAB)
+    action2.send_keys(Keys.TAB)
+    action2.send_keys(Keys.ENTER)
+    action2.send_keys(Keys.ENTER)
+    action2.perform()
+    time.sleep(2)
+
+
+    driver2.quit()
+"""
